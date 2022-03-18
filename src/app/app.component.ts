5d686c5ae3468049ad9ade4f1767a57ee13b529c
meluferiev <meluferiev@axbit.ru>
2022-03-18 17:50:28 +0400

    feat: form refresh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-services';
+export class AppComponent implements OnInit {
+  form: FormGroup;
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+      address: new FormGroup({
+        country: new FormControl('kz'),
+        city: new FormControl('Astana', Validators.required)
+      })
+    });
+  }
+
+  submit() {
+    if (this.form.valid) {
+      const formData = {...this.form.value};
+    }
+  }
+
+  setCapital() {
+    const cityMap = {
+      ru: 'Moscow',
+      ua: 'Kiev',
+      by: 'Minsk',
+      kz: 'Astana'
+    }
+
+    const cityKey: 'ru' | 'ua' | 'by' | 'kz' = this.form.get('address')!.get('country')!.value;
+    const city = cityMap[cityKey];
+
+    this.form.patchValue({
+      address: {city}
+    });
+  }
 }
